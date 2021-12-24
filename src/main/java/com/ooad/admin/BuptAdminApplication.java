45d963ed0bcd48027fa5f9fbade728f2ec614e6b
huyang <michael531@bupt.edu.cn>
2021-12-24 13:59:15 +0800

    Initial commit

---
+++ src/main/java/com/ooad/admin/BuptAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ooad.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuptAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuptAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooad/admin/BuptAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuptAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
