7dfe840a0303adc4cd36a4ac4d54f53b894591c8
翰成 吴 <939699895@qq.com>
2022-01-07 14:20:18 +0800

    根据学生的增与查使用的数据模型更改为apifox上定义的数据模型「学生」

--- src/main/java/com/ooad/admin/controller/AdminController.java
+++ src/main/java/com/ooad/admin/controller/AdminController.java
@@ -3,31 +3,30 @@ package com.ooad.admin.controller;
 import com.ooad.admin.crudrepository.StudentRepository;
 import com.ooad.admin.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudentController {
+@RequestMapping("/admin")
+public class AdminController {
     @Autowired
     private StudentRepository studentRepository;
-    /**
-     * 新增一条数据
-     * @param name
-     * @return
-     */
-    @RequestMapping("/add")//http://localhost:8080/add?name=test123&year=2024&id=1
-    public Student add(String name,String id,String year){
-        Student student = new Student();
-        student.setName(name);
-        student.setYear(year);
-        student.setId(id);
+
+    @RequestMapping("/stuadmin/add")
+    public Student add(@RequestBody Student student){
+
+//        student.setYear(year);
+//        student.setId(id);
         return studentRepository.save(student);
     }
     /**
      * 查询所有数据
      * @return
      */
-    @RequestMapping("/list")//http://localhost:8080/list
+    @ResponseBody
+    @RequestMapping("/stuadmin/list")//http://localhost:8080/stuadmin/list
     public Iterable<Student> list(){
         Iterable<Student> all = studentRepository.findAll();
         return all;
--- src/main/java/com/ooad/admin/model/Student.java
+++ src/main/java/com/ooad/admin/model/Student.java
@@ -1,5 +1,6 @@
 package com.ooad.admin.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -10,24 +11,31 @@ public class Student {
 
     //    定义主键id
     @Id
-    private String id;
+    @Column(name="sid")
+    private String sid;
 
     @Column(name="name")
     private String name;
 
-    @Column(name="year")
-    private String year;
+    @Column(name="sex")
+    private String sex;
 
-    public Student() {
+    @Column(name="province")
+    private String province;
 
+    @Column(name="ID")//命名有大写情况的时候可能会出现传入null值的问题，
+    //// 这个时候需要改名或者在getter和setter加上notation" @JsonProperty(value = "ID") "
+    private String ID;
+
+    @Column(name="department")
+    private String department;
+
+    public String getSid() {
+        return sid;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    public void setSid(String sid) {
+        this.sid = sid;
     }
 
     public String getName() {
@@ -38,18 +46,51 @@ public class Student {
         this.name = name;
     }
 
-    public String getYear() {
-        return year;
+    public String getSex() {
+        return sex;
     }
 
-    public void setYear(String year) {
-        this.year = year;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
-    public Student(String id, String name, String year) {
-        this.id = id;
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    @JsonProperty(value = "ID")
+    public String getID() {
+        return ID;
+    }
+    @JsonProperty(value = "ID")
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Student(String sid, String name, String sex, String province, String ID, String department) {
+        this.sid = sid;
         this.name = name;
-        this.year = year;
+        this.sex = sex;
+        this.province = province;
+        this.ID = ID;
+        this.department = department;
     }
+
+    public Student() {
+
+    }
+
+
 
 }
