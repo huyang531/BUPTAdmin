445b60edad326ee39a89f92a48347708b5772ba4
翰成 吴 <939699895@qq.com>
2022-01-08 17:29:52 +0800

    fix(backend): add OneToMany notation on field Course.studentlist feat(backend): implement CRUD functionality of Course and Classroom

--- src/main/java/com/ooad/admin/model/Course.java
+++ src/main/java/com/ooad/admin/model/Course.java
@@ -2,10 +2,9 @@ package com.ooad.admin.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="course")
@@ -36,14 +35,8 @@ public class Course {
     @Column(name="day")
     private String day;
 
-    public Course(String course, String name, String teacher, String credit, String department, String day) {
-        this.course = course;
-        this.name = name;
-        this.teacher = teacher;
-        this.credit = credit;
-        this.department = department;
-        this.day = day;
-    }
+    @OneToMany
+    private Set<Student> studentlist = new HashSet<Student>();
 
     public Course() {
     }
@@ -94,5 +87,13 @@ public class Course {
 
     public void setDay(String day) {
         this.day = day;
+    }
+
+    public Set<Student> getStudentlist() {
+        return studentlist;
+    }
+
+    public void setStudentlist(Set<Student> studentlist) {
+        this.studentlist = studentlist;
     }
 }
