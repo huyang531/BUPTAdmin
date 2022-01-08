9bde0fe8ea7a38cd7d67be8c9ff208cfda9e8533
huyang <michael531@bupt.edu.cn>
2022-01-08 17:00:13 +0800

    refactor(backend): refactor Course.java

--- src/main/java/com/ooad/admin/model/Course.java
+++ src/main/java/com/ooad/admin/model/Course.java
@@ -1,6 +1,10 @@
 package com.ooad.admin.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,6 +12,10 @@ import java.util.List;
 
 @Entity
 @Table(name="course")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
 
     @Id
@@ -40,63 +48,4 @@ public class Course {
     @JoinColumn(name="studentlist")
     private List<Student> studentlist = new ArrayList<>();
 
-    public Course(String course, String name, String teacher, String credit, String department, String day) {
-        this.course = course;
-        this.name = name;
-        this.teacher = teacher;
-        this.credit = credit;
-        this.department = department;
-        this.day = day;
-    }
-
-    public Course() {
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
-    }
-
-    public String getCredit() {
-        return credit;
-    }
-
-    public void setCredit(String credit) {
-        this.credit = credit;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
 }
