1ce42411bf55a6a97501afcb3717d2af992c790d
Tro10086 <81070165+Tro10086@users.noreply.github.com>
2022-01-09 04:50:17 +0800

    Add files via upload

---
+++ front-end/sa-teacher/sa-frame/menu-list.js
@@ -0,0 +1,30 @@
+var menuList =	[
+	{
+		id: '2',
+		name: '查询信息',
+		icon: 'el-icon-collection-tag',
+		info: '增删改查教师信息',
+		url: 'sa-view/teacher/teacher-manage.html'
+	},
+	{
+		id: '3',
+		name: '查看学生',
+		icon: 'el-icon-user',
+		info: '查看学生信息',
+		url: 'sa-view/student/student-manage.html'
+	},
+	{
+		id: '4',
+		name: '查看课程',
+		icon: 'el-icon-data-line',
+		info: '查看课程信息',
+		url: 'sa-view/course/course-manage.html'
+	},
+	{
+		id: '5',
+		name: '查看课表',
+		icon: 'el-icon-search',
+		info: '查看课表',
+		url: 'sa-view/timetable/timetable.html'
+	}
+]
